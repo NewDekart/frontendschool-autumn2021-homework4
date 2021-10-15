6723241c2266aedb77280e0d830e7d072c7fddbb
Amal Dusalin <adusalin@fil-it.ru>
2021-10-15 22:08:57 +0300

    Перевел операцию сравнения в асинхронный вид

--- solution/index.js
+++ solution/index.js
@@ -16,25 +16,29 @@ module.exports = function (Homework) {
         Homework.subtract(a, b, (result) => resolve(result))
     })
 
+    const lessOrEqual = (a, b) => new Promise((resolve) => {
+        Homework.lessOrEqual(a, b, (result) => resolve(result))
+    })
+
     return async (array, fn, initialValue, cb) => {
 
         const length = await getArrayLength(array)
         const lastIndex = await subtract(length, 1)
         let currentItemIndex = 0
         let result = initialValue
+		let shouldContinue = await lessOrEqual(currentItemIndex, lastIndex)
 
-        while (currentItemIndex <= lastIndex) {
+        while (shouldContinue) {
             const currentItem = await getArrayItemByIndex(array, currentItemIndex)
             result = await new Promise((resolve) => {
                 fn(result, currentItem, currentItemIndex, array, (result) => resolve(result))
             })
             currentItemIndex = await add(currentItemIndex, 1)
+			shouldContinue = await lessOrEqual(currentItemIndex, lastIndex)
         }
 
         if (cb) {
             cb(result)
-
-            return
         }
 
         return result
